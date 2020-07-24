fc97f0b57572dfb9e7218dcb54248bd86f4b5834
mbknu <bouchelot.m@gmail.com>
2020-07-24 10:55:29 +0200

    add todo route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const facts = require("./facts");
 const teams = require("./teams");
 const auth = require("./auth");
+const todo = require("./todo");
 
 router.use("/facts", facts);
 router.use("/teams", teams);
 router.use("/auth", auth);
+router.use("/todo", todo);
 
 module.exports = router;
